50e0389fd0ab7e1a782579b10b302dd0bd286b5d
mandy <mcyeekyun@gmail.com>
2023-09-21 00:48:57 +0800

    change 16, 17, 18

--- src/List/components/ItemDetails.js
+++ src/List/components/ItemDetails.js
@@ -1,72 +1,76 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useMemo } from "react";
+import debounce from 'lodash.debounce';
 import { useParams, useNavigate } from "react-router-dom";
 import classnames from "classnames";
 import css from "../list.module.scss";
 
 import { ThemeContext, ListContext } from "../../context";
 
+const getIndex = (list, key, value) => {
+  return list.findIndex((item) => {
+    if (item.subList[0]) {
+      getIndex(item.subList, key, value);
+    }
+    return item[key] === value;
+  });
+};
+
 export default function ItemDetails() {
   const { theme } = useContext(ThemeContext);
   const { list, setList } = useContext(ListContext);
-  const [details, setDetails] = useState("");
   const params = useParams();
   const navigate = useNavigate();
 
-  const getIndex = (list, key, value) => {
-    return list.findIndex((item) => {
-      if (item.subList[0]) {
-        getIndex(item.subList, key, value);
-      }
-
-      return item[key] === value;
-    });
-  };
-
   const itemIdx = params?.itemId && getIndex(list, "id", params.itemId);
-  const item = itemIdx !== -1 && list[itemIdx];
-
+  const item = list[itemIdx];
+  const [details, setDetails] = useState(list[itemIdx]?.remarks);
+  
   useEffect(() => {
     if (list.length === 0) {
       navigate("/");
     }
-    const existingRemarks = list && list[itemIdx]?.remarks;
-    setDetails(existingRemarks);
+  }, [list]);
 
-    return () => setDetails("");
-  }, [itemIdx, list]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const remarks = details;
+  const updateData = (e) => {
     const updatedList = [...list];
-    if (itemIdx !== -1 && updatedList) {
-      updatedList[itemIdx].remarks = remarks || "";
-    }
+    const itemIdx = params?.itemId && getIndex(list, "id", params.itemId);
+    if (itemIdx !== -1) updatedList[itemIdx].remarks = e.target.value;
     setList(updatedList);
-    navigate("/");
-  };
+  }
+
+  const changeHandler = useMemo(() => debounce(updateData, 500), []);
 
   const handleChange = (e) => {
     setDetails(e.target.value);
-  };
+    changeHandler(e);
+  }
+
+  useEffect(() => {
+    return () => changeHandler.cancel()
+  }, [changeHandler])
 
   const handleCompleted = (e) => {
     e.preventDefault();
     const updatedList = [...list];
-
-    item.status = "completed";
-    updatedList.splice(itemIdx, 1)
-    updatedList.push(item);
-
+    const itemIdx = params?.itemId && getIndex(list, "id", params.itemId);
+    if (itemIdx !== -1) updatedList[itemIdx].status = "completed";
     setList(updatedList);
-    navigate("/");
   };
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    const updatedList = [...list];
+    const itemIdx = params?.itemId && getIndex(list, "id", params.itemId);
+    updatedList.splice(itemIdx, 1);
+    setList(updatedList);
+    navigate("/")
+  }
 
   return (
     <div className={css.itemDetails}>
       <div className={css.itemInfo}>
         <div>上層任務</div>
-        <div className={`gray`}>吃飯 / 炒類</div> {/* upper level task */}
+        <div className={`gray`}>無</div> {/* upper level task */}
       </div>
 
       <div className={css.itemInfo}>
@@ -79,31 +83,29 @@ export default function ItemDetails() {
         <div>備註</div>
       </div>
 
-      <form name="itemDetails" onSubmit={(e) => handleSubmit(e)}>
+      <form name="itemDetails" onSubmit={(e) => handleCompleted(e)}>
         <textarea
           rows="10"
           columns="10"
           className={css.detailsInput}
           placeholder="輸入備註"
-          onChange={(e) => {
-            handleChange(e);
-          }}
+          onChange={(e) => handleChange(e)}
           value={details}
         />
+        {item && item.status === "pending" && (
         <button
           type="submit"
           className={classnames(css.detailsButton, `bg-main-${theme}`)}
         >
           完成
         </button>
-        {item && item.status === "pending" && (
-          <button
-            onClick={(e) => handleCompleted(e)}
-            className={classnames(css.detailsButton, `bg-main-red`)}
-          >
-            刪除
-          </button>
         )}
+        <button
+          onClick={(e) => handleDelete(e)}
+          className={classnames(css.detailsButton, `bg-main-red`)}
+        >
+          刪除
+        </button>
       </form>
     </div>
   );
